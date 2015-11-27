ea3f6889686111724911ce6f75e6c0c91f886140
Emil Karlsson <ek222mw@student.lnu.se>
2015-11-27 13:50:35 +0100

    ...

--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -2,14 +2,49 @@ var initialState = require('./../initialstate');
 
 var QuizReducer = function(state, action){
     var newState = Object.assign({}, state);
+	
+	
+	
+	var quizObj = {quests:["In which band was Bon Scott the singer?", "In which NHL team did Teemu Selänne play?"],
+	opt:["Iron Maiden","Metallica","AC/DC", "Chicago Blackhawks","Anaheim Mighty Ducks", "Boston Bruins"],
+	CorrAns:["Ans3","Ans3","Ans2"]};
+	
+	
     switch(action.type){
         case 'Pressed':
-            if(action.answer === "Ans3")
+            if(action.answer === quizObj.CorrAns[newState.CountAnswers])
 			{
+				newState.Points++;
+				newState.CountAnswers++;;
 				newState.currentValue = "Correct";
+				
+				newState.Choice1 = quizObj.opt[newState.CountChoices];
+				newState.CountChoices++;
+				
+				newState.question = quizObj.quests[newState.CountQuestions];
+				newState.CountQuestions++;
+				
+				newState.Choice2 = quizObj.opt[newState.CountChoices];
+				newState.CountChoices++;
+				
+				newState.Choice3 = quizObj.opt[newState.CountChoices];
+				newState.CountChoices++;
 			}
 			else{
 				newState.currentValue = "False";
+				newState.CountAnswers++;;
+				
+				newState.Choice1 = quizObj.opt[newState.CountChoices];
+				newState.CountChoices++;
+				
+				newState.question = quizObj.quests[newState.CountQuestions];
+				newState.CountQuestions++;
+				
+				newState.Choice2 = quizObj.opt[newState.CountChoices];
+				newState.CountChoices++;
+				
+				newState.Choice3 = quizObj.opt[newState.CountChoices];
+				newState.CountChoices++;
 			}
             return newState;
         default:
